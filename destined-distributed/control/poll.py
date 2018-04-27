9b3b5c1c5c9a9bb6d7d1b347e15ca467ba3c5427
Simon Bowly <simon.bowly@gmail.com>
2018-04-27 16:57:28 +1000

    Pretty state polling (for use with watch).

--- destined-distributed/control/poll.py
+++ destined-distributed/control/poll.py
@@ -8,15 +8,29 @@ import zmq
 import protocol
 
 
+template = '''
+Node:       {name}
+State:      {state}
+Workers:    {workers_all} ({workers_up} up, {workers_down} down)
+CPU Util:   {cpu_percent:>5.1f} %
+Mem Util:   {mem_percent:>5.1f} %
+'''
+
+
 context = zmq.Context()
 
 controller_address = 'tcp://localhost:6000'
 
 socket = context.socket(zmq.REQ)
 socket.connect(controller_address)
+socket.send(protocol.msg_request_state())
+response = socket.recv()
 
-while True:
-    socket.send(protocol.msg_request_state())
-    response = socket.recv()
-    pprint(protocol.decode(response))
-    time.sleep(1)
+controller_state = protocol.decode(response)
+
+for node in controller_state['nodes'].values():
+    print(template.format(
+        workers_all=len(node['workers']),
+        workers_up=sum(worker['up'] for worker in node['workers']),
+        workers_down=sum(not worker['up'] for worker in node['workers']),
+        **node))
