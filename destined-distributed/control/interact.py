a7445843fc10aa52770d14a8113e2a33cc480610
Simon Bowly <simon.bowly@gmail.com>
2018-07-02 09:14:08 +1000

    Poll and shutdown scripts for remote admin.

--- destined-distributed/control/interact.py
+++ destined-distributed/control/interact.py
@@ -1,5 +1,6 @@
 ''' Query system state, send control signals. '''
 
+import click
 import IPython
 import zmq
 
@@ -7,7 +8,8 @@ import protocol
 
 context = zmq.Context()
 
-controller_address = 'tcp://localhost:6000'
+controller_address = 'tcp://192.168.1.106:6000'
+
 
 def client_send_recv(request):
     socket = context.socket(zmq.REQ)
@@ -22,14 +24,32 @@ def state():
     response = client_send_recv(request)
     return protocol.decode(response)
 
-def shutdown(node):
+def shutdown_node(node):
     request = protocol.msg_request_shutdown(node)
     response = client_send_recv(request)
     return response
 
-print('''
-state()         -> Print the system state.
-shutdown(node)  -> Signal the system to shut down.
-''')
 
-IPython.embed()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def interact():
+
+    print('''
+    state()         -> Print the system state.
+    shutdown(node)  -> Signal the system to shut down.
+    ''')
+
+    IPython.embed()
+
+
+@cli.command()
+def shutdown():
+    click.echo({node['name']: shutdown_node(_id) for _id, node in state()['nodes'].items()})
+
+
+if __name__ == '__main__':
+    cli()
--- destined-distributed/control/poll.py
+++ destined-distributed/control/poll.py
@@ -1,6 +1,9 @@
-''' Query system state, send control signals. '''
+#!/home/simon/.virtualenvs/py36/bin/python
 
-from pprint import pprint
+import datetime
+import json
+import operator
+import os
 import time
 
 import zmq
@@ -19,18 +22,57 @@ Mem Util:   {mem_percent:>5.1f} %
 
 context = zmq.Context()
 
-controller_address = 'tcp://localhost:6000'
+sink = context.socket(zmq.PULL)
+sink.bind('tcp://*:6010')
+recv_count = 0
+lines_written = 0
+output_file = None #'/tmp/destined.out'
+output_buffer = []
 
-socket = context.socket(zmq.REQ)
-socket.connect(controller_address)
-socket.send(protocol.msg_request_state())
-response = socket.recv()
+controller_address = 'tcp://192.168.1.106:6000'
+last_poll = datetime.datetime.fromtimestamp(0)
+period_seconds = 1
 
-controller_state = protocol.decode(response)
+if output_file:
+    mode = f'-> {output_file}'
+else:
+    mode = '(discarding results)'
 
-for node in controller_state['nodes'].values():
-    print(template.format(
-        workers_all=len(node['workers']),
-        workers_up=sum(worker['up'] for worker in node['workers']),
-        workers_down=sum(not worker['up'] for worker in node['workers']),
-        **node))
+while True:
+
+    waiting = sink.poll(timeout=period_seconds)
+    if waiting:
+        message = sink.recv()
+        result = protocol.decode(message)
+        output_buffer.append(json.dumps(result))
+        recv_count += 1
+
+    now = datetime.datetime.now()
+    if (now - last_poll) >= datetime.timedelta(seconds=period_seconds):
+        last_poll = now
+
+        if output_file:
+            with open(output_file, 'a') as outfile:
+                outfile.writelines(output_buffer)
+            lines_written += len(output_buffer)
+            assert lines_written == recv_count
+        output_buffer = []
+
+        socket = context.socket(zmq.REQ)
+        socket.connect(controller_address)
+        socket.send(protocol.msg_request_state())
+        response = socket.recv()
+
+        controller_state = protocol.decode(response)
+        nodes = sorted(controller_state['nodes'].values(), key=operator.itemgetter('name'))
+
+        os.system('clear')
+        print(f'========== {now.strftime("%Y-%m-%d %H:%M:%S")} ==========')
+        print(f'\nReceived messages: {recv_count} {mode}')
+
+        for node in nodes:
+            print(template.format(
+                workers_all=len(node['workers']),
+                workers_up=sum(worker['up'] for worker in node['workers']),
+                workers_down=sum(not worker['up'] for worker in node['workers']),
+                **node))
